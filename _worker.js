6f807c4310eba09484c5b699275bdc224c72a7ed
gianmattiadecarlo <134106119+gianmattiadecarlo@users.noreply.github.com>
2024-10-01 11:40:42 +0330

    _worker.js

--- _worker.js
+++ _worker.js
@@ -7,7 +7,7 @@ import { connect } from 'cloudflare:sockets';
 
 // How to generate your own UUID:
 // https://www.uuidgenerator.net/
-let userID = '89b3cbba-e6ac-485a-9481-976a0415eab9';
+let userID = '8f0956f4-d736-4aba-9380-50a359272af7';
 
 // https://www.nslookup.io/domains/bpb.yousef.isegaro.com/dns-records/
 const proxyIPs= ['bpb.yousef.isegaro.com'];
